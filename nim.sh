6bc5ca3d90246942b95dc0767124d430903a5000
Maria Montagu <mafemont16@gmail.com>
2020-11-22 22:29:02 +0100

    added a few lines to make it prettier

--- nim.sh
+++ nim.sh
@@ -1,52 +1,59 @@
 #!/bin/bash
 
 echo ""
-echo "*****************************************************"
+echo "-----------------------------------------------------"
 echo "*****************************************************"
 echo ">>------>******Welcome to the Nim Game******<------<<"
 echo "*****************************************************"
-echo "*****************************************************"
+echo "-----------------------------------------------------"
 echo ""
 echo "Hello there! What's your name?"
 echo ""
 read name
 echo ""
 echo "Welcome to the Nim game (^_^), $name"
-echo "*****************************************************"
+echo "----------------------------------------------------"
 
 
 nb_items=20
 
 while [ $nb_items -gt 0 ]
 do
-    #Turn of PLAYER 1
+    #PLAYER'S TURN
         echo ""
-        echo "It's $name's turn:"
-        echo "There are $nb_items items: ||||||||||||||||||||"
-        echo "How many items do you want take [between 1 - 3] ?"
+        echo "*****************************************************"
+        echo "*********>>------>It's $name's turn<------<<*********"
+        echo "*****************************************************"
+        echo ""
+        echo "There are $nb_items wooden sticks: ||||||||||||||||||||"
+        echo "How many do you want take [between 1 - 3] ?"
         read player_choice
         while [ $player_choice -gt 3 ] || [ $player_choice -lt 1 ]
         do
-            echo "Please choose again, how many items do you want take [between 1 - 3] ?"
+            echo "Please choose again, how many sticks do you want take [between 1 - 3] ?"
             read player_choice
             if [ $player_choice -gt 3 ] || [ $player_choice -lt 1 ]
             then
                 continue
             fi
         done
-        echo "$name, you take $player_choice"
+        echo "$name, you take $player_choice sticks"
         let "nb_items=$nb_items - $player_choice"
-        echo "Now there are: $nb_items left"
+        echo "Now there are: $nb_items sticks left"
         echo ""
-    #Turn of IA - computer
-        echo "Computer's turn: "
-        echo "There are $nb_items items: ||||||||||||||||||||"
-        let "ai_choice=4 - $player_choice"
-        echo "Computer, you took $ai_choice"
+      #COMPUTER'S TURN
+        echo ""
+        echo "*****************************************************"
+        echo "******>>------>It's the computer's turn<------<<*****"
+        echo "*****************************************************"
+        echo ""
+        echo "There are $nb_items wooden sticks: ||||||||||||||||||||"
+        let "ai_choice= 4 - $player_choice"
+        echo "Computer took $ai_choice sticks"
         let "nb_items=$nb_items-$ai_choice"
-        echo "Now there are: $nb_items"
+        echo "Now there are: $nb_items sticks left"
         if [ $nb_items -eq 0 ]
         then
-            echo "You lost!"
+            echo "{-_-} You lost!"
         fi
 done
